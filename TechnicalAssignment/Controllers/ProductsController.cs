81cba84dc9fa63c71e2d472464f983486f09faea
Joao Monteiro <jna.monteiro@gmail.com>
2021-12-14 23:29:13 +0100

    Add products controller

---
+++ TechnicalAssignment/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TechnicalAssignment.Data.Models;
+using TechnicalAssignment.Services;
+
+namespace TechnicalAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAsync()
+        {
+            IEnumerable<ProductDto> products = await productsService.GetAllProductsAsync();
+
+            return Ok(products);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetAsync(int id)
+        {
+            ProductDto order = await productsService.GetProductAsync(id);
+
+            return Ok(order);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
