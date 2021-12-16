21012b7b6ecd4db8154697c17eaec611360b10e3
Joao Monteiro <jna.monteiro@gmail.com>
2021-12-16 17:23:16 +0100

    Add products controller documentation

--- TechnicalAssignment/Controllers/ProductsController.cs
+++ TechnicalAssignment/Controllers/ProductsController.cs
@@ -11,20 +11,31 @@ using TechnicalAssignment.Services.Models;
 
 namespace TechnicalAssignment.Controllers
 {
+    /// <summary>
+    /// This class implements the products controller.
+    /// It contains methods to get products from the database.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
         private readonly IProductsService productsService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductsController"/> class.
+        /// </summary>
+        /// <param name="productsService">Service used in the controller.</param>
         public ProductsController(IProductsService productsService)
         {
             this.productsService = productsService;
         }
 
+        /// <summary>
+        /// Gets all products.
+        /// </summary>
+        /// <returns>Products retrieved from database.</returns>
         [HttpGet]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<ProductDto>))]
-        [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ActionResult))]
         public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllAsync()
         {
             IEnumerable<ProductDto> products = await productsService.GetAllProductsAsync();
@@ -32,6 +43,11 @@ namespace TechnicalAssignment.Controllers
             return Ok(products);
         }
 
+        /// <summary>
+        /// Gets a single product.
+        /// </summary>
+        /// <param name="id">Product ID.</param>
+        /// <returns>Product retrieved from database. If the product does not exist, a <see cref="NotFoundResult"/> is returned instead.</returns>
         [HttpGet("{id}")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ProductDto))]
         [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ActionResult))]
